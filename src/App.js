0b231ff0edbbe6bb9054b34ac409bff058564389
neo1415 <adedaniel502@gmail.com>
2022-06-02 10:08:00 +0100

    Uriel Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Header, About, Services, Team, Testimonial, Footer } from './Containers';
+import { NavBar } from './Components';
+import './App.scss'
+import Mission from './Containers/Mission1/Mission1';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <NavBar />
+      <Header />
+      <About />
+      <Mission />
+      <Services />
+      <Team />
+      <Testimonial />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Containers/index.js
@@ -0,0 +1,9 @@
+export { default as Header } from './Header/Header';
+export { default as About } from './About/About';
+export { default as Services } from './Services/Services';
+export { default as Team } from './Team/Team';
+export { default as Testimonial } from './Testimonial/Testimonial';
+export { default as Footer } from './Footer/Footer';
+
+
+
